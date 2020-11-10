d95cc43d3c3a93e52e425039489693737d1d9615
jeevankc91 <jeevankc91@gmail.com>
2020-11-10 22:47:26 +0530

    gpio specific driver files and HSI mesurement new project

---
+++ Stm32_Driver/Driver/Inc/stm32f446xx_gpio_driver.h
@@ -0,0 +1,14 @@
+/*
+ * stm32f446xx_drive.h
+ *
+ *  Created on: 10-Nov-2020
+ *      Author: g1
+ */
+
+#ifndef INC_STM32F446XX_GPIO_DRIVER_H_
+#define INC_STM32F446XX_GPIO_DRIVER_H_
+
+#include "stm32f446xx.h"
+
+
+#endif /* INC_STM32F446XX_GPIO_DRIVER_H_ */
---
+++ Stm32_Driver/Driver/Src/stm32f446xx_gpio_driver.c
@@ -0,0 +1,9 @@
+/*
+ * stm32f446xx_drive.c
+ *
+ *  Created on: 10-Nov-2020
+ *      Author: g1
+ */
+
+
+#include "stm32f446xx_gpio_driver.h"
